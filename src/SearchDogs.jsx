c280da1eac85a6deab5eddd4059b365261ca8ceb
wilson <wilsnyeh@gmail.com>
2023-05-22 09:08:45 -0700

    coats and colors not defined??

--- src/SearchDogs.jsx
+++ src/SearchDogs.jsx
@@ -1,15 +1,38 @@
 import React, { useState } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function SearchDogs({token}) {
-  const [dogCoat, setDogCoat] = useState("");
-  const [dogColor, setDogColor] = useState("");
+  
+  const [dogCoat, setDogCoat] = useState(["Hairless", "Short", "Medium", "Long", "Wire", "Curly"].map(coat=>({name: coat, checked:false})));
+  const [dogColor, setDogColor] = useState([
+    "Apricot / Beige",
+    "Bicolor",
+    "Black",
+    "Brindle",
+    "Brown / Chocolate",
+    "Golden",
+    "Gray / Blue / Silver",
+    "Harlequin",
+    "Merle (Blue)",
+    "Merle (Red)",
+    "Red / Chestnut / Orange",
+    "Sable",
+    "Tricolor (Brown, Black, & White)",
+    "White / Cream",
+    "Yellow / Tan / Blond / Fawn",
+  ].map(color => ({name: color, checked:false})));
 
-  const handleDogCoatChange = (e) => {
-    if (e.target.checked) {
-        setDogCoat();
-    }
+  // const handleDogCoatChange = (e) => {
+  //   if (e.target.checked) {
+  //       setDogCoat();
+  //   }
+  // }
+
+  const handleChecked = (list, setList) => e => {
+    const {name, checked } = e.target;
+    setList(prevList => prevList.map(item=>item.name === name? {...item} : item))
   }
+
 
   let navigate = useNavigate();
   const routeChange = () => {
@@ -48,7 +71,10 @@ function SearchDogs({token}) {
   };
 const SearchForDogs = () => {
 
-  let breedSearchUrl =`https://api.petfinder.com/v2/types/animals?type=dog&coat=short&colors=black`;
+  const selectedCoats = dogCoats.filter(coat => coat.checked).map(coat=> coat.name)
+  const selectedColors = dogColors.filter(color=> color.checked).map(color =>color.name)
+
+  let breedSearchUrl = `https://api.petfinder.com/v2/types/animals?type=dog${coat ? `&coat=${coat}`: ""}${color ? `&colors=${color}`: ""}`;
   let breedSearchOptions = {     
       method: 'GET',
       headers: {
@@ -71,10 +97,10 @@ const SearchForDogs = () => {
         <h2>coat</h2>
       </div>
       <div style={columns}>
-        {params.coats.map((coat) => (
-          <div>
-            <label htmlFor="coat">{coat}</label>
-            <input type="checkbox" id="coat"></input>
+        {dogCoat.map((coat, i) => (
+          <div key={i}>
+            <label htmlFor={coat.name}>{coat.name}</label>
+            <input type="checkbox" id={coat.name} checked={coat.checked} onChange={handleChecked(dogCoat, setDogCoat)}></input>
           </div>
         ))}
       </div>
