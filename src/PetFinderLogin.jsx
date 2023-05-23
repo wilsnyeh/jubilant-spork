25d7df0750de8a186609cb37b8a91a03a9bdb453
wilson <wilson@looptifyapp.com>
2023-05-23 14:20:11 -0700

    petfinderlogin updates, still working

--- src/PetFinderLogin.jsx
+++ src/PetFinderLogin.jsx
@@ -1,65 +1,63 @@
-import React, { useState } from "react";
-import { json, useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import SearchBreeds from "./SearchBreeds";
 import SearchDogs from "./SearchDogs";
 
-
-
 const PetFinderLogin = () => {
-  const [token, setToken] = useState('');
-
+  const [token, setToken] = useState("");
+  // const [colors, setColors] = useState("");
+  // const [coats, setCoats] = useState("");
+  const [data, setaData] = useState([]);
 
   let navigate = useNavigate();
   const routeChange = () => {
-      let path = `/searchdogs/`
-      navigate(path);
-  }
-
+    let path = `/searchdogs/`;
+    navigate(path);
+  };
 
   async function loginUser() {
     let petFinderUrl = "https://api.petfinder.com/v2/oauth2/token";
-    // console.log('what is this? line20 process.env', process.env)
-  
+
     fetch(petFinderUrl, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({"grant_type":"client_credentials","client_id": "cWJAoFN567mRPOc10isZZMomu17mBvGlbEyRrSZOYLJZspuR0w","client_secret":"N59RE4DeVfX9pCLIUBzR66nwXpxqGU0kGCbsV126"})
+      body: JSON.stringify({
+        grant_type: "client_credentials",
+        client_id: "cWJAoFN567mRPOc10isZZMomu17mBvGlbEyRrSZOYLJZspuR0w",
+        client_secret: "N59RE4DeVfX9pCLIUBzR66nwXpxqGU0kGCbsV126",
+      }),
     })
       .then((res) => {
-        return res.json()
-    })
+        return res.json();
+      })
       .then((json) => {
-        console.log('this is the json line 32',json);
+        console.log(json);
         setToken(json.access_token);
-    })
+      })
       .catch((err) => console.error("!!loginUser Error!!", err));
-      console.log('is this the token? line37', token)
+    console.log("is this the token? line37", token);
   }
 
   const SearchForDogs = () => {
-
-    let breedSearchUrl =`https://api.petfinder.com/v2/animals?type=dog&colors=black&coat=short`;
-    let breedSearchOptions = {     
-        method: 'GET',
-        headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`
-      }
-  
-    }
+    let breedSearchUrl = `https://api.petfinder.com/v2/animals?type=dog&colors=black&coat=short`;
+    let breedSearchOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
     fetch(breedSearchUrl, breedSearchOptions)
-    .then(res => res.json())
-    .then(json => console.log(json))
-    .then(res => console.log('this is line55', JSON.stringify(res)))
-    .catch(err => console.error('error:' + err));
-    console.log('now what is token?: line 64', token)
-    console.log('is this the list of search results?', )
+      .then((res) => res.json())
+      .then((json) =>
+        console.log("how do i capture this data?", json, JSON.stringify(json)))
+      // .then((data) => )
+      .catch((err) => console.error("error:" + err));
 
-    // let content = res.json()
-  }
-
+    // let content =
+  };
 
   // let navigate = useNavigate();
   // const routeChange = () => {
@@ -76,7 +74,20 @@ const PetFinderLogin = () => {
   //     setToken(token)
   // }
 
-
+  const searchInput = () => {
+    let breedSearchUrl = `https://api.petfinder.com/v2/animals?type=dog&location=california&breed=bulldog`;
+    let breedSearchOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    fetch(breedSearchUrl, breedSearchOptions)
+      .then((res) => res.json())
+      .then((json) => console.log("this is the json", json))
+      .catch((err) => console.error("error:" + err));
+  };
 
   return (
     <div>
@@ -84,15 +95,44 @@ const PetFinderLogin = () => {
         <button
           // onClick={() => login(userName, password)}
           type="submit"
-          onClick={()=> {
+          onClick={() => {
             loginUser();
             // routeChange();
-          }}>
+          }}
+        >
           gib token
         </button>
-        <SearchBreeds token={token}/>
+        <SearchBreeds token={token} />
         {/* <SearchDogs token={token}/> */}
-        <button onClick={SearchForDogs}>click this for other dogs</button>
+
+        <input type="search" placeholder="location" />
+        <br></br>
+        <input type="search" placeholder="dog breed" />
+        <button type="submit" onClick={searchInput}>
+          submit
+        </button>
+        <br></br>
+
+        <button onClick={SearchForDogs}>this is search for dogs</button>
+        <div>
+          {/*
+          we want to check for token here, if token is available, we can begin render data
+          for user 
+          */}
+          {/* {data ? (
+            <div>
+              {data.animals.map((animal) => {
+                return (
+                  <div>
+                    <div>{animal.location}</div>
+                    <div>{animal.breed}</div>
+                    
+                  </div>
+                );
+              })}
+            </div>
+          ) : null} */}
+        </div>
       </div>
     </div>
   );
